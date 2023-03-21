7c8169b696fc620a7c774519d04e16386d6c6afd
Yunsung Kim <yunsungkim0908@gmail.com>
2023-03-21 06:23:16 -0700

    small fix

--- zipf_inference/theory/utils.py
+++ zipf_inference/theory/utils.py
@@ -7,9 +7,8 @@ def get_sorted_counts(name):
 
     sorted_vals_name = name + 'sorted_vals.bin'
     if os.path.isfile(sorted_vals_name):
-        return np.fromfile(sorted_vals_name)
+        return np.fromfile(sorted_vals_name, dtype=np.int32)
     a = np.fromfile(name + 'vals.bin', dtype=np.int32)
     a[::-1].sort()
     a.tofile(sorted_vals_name)
     return a
-    
